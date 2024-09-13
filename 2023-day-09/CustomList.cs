e9904c8ca14dd97b1906accdef2bacd189db99d1
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-09-13 13:37:34 -0500

    Solve part 1

---
+++ 2023-day-09/CustomList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_day_09;
+
+public class CustomList : List<long>
+{
+    private HashSet<long> _values;
+    public int UniqueElements => _values.Count;
+
+    public CustomList(IEnumerable<long> l) : base(l)
+    {
+        _values = new HashSet<long>(l);
+    }
+
+    public CustomList() : base()
+    {
+        _values = new();
+    }
+
+    new public void Add(long value)
+    {
+        base.Add(value);
+        _values.Add(value);
+    }
+
+    public bool IsAllZeroes() => _values.Count == 1 && this[0] == 0;
+
+    public override string ToString()
+    {
+        return $"[ {string.Join(", ", this)} ]";
+    }
+}
--- 2023-day-09/Program.cs
+++ 2023-day-09/Program.cs
@@ -4,6 +4,76 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool useTestData = false;
+        IList<string> data = useTestData ? GetTestData() : GetPuzzleInput();
+        long result = 0;
+        foreach (string puzzle in data)
+        {
+            result += GetPuzzleAnswer(puzzle);
+        }
+        Console.WriteLine(result);
+    }
+
+    static IList<string> GetTestData()
+    {
+        return [
+            "0 3 6 9 12 15",
+            "1 3 6 10 15 21",
+            "10 13 16 21 30 45"
+            ];
+    }
+
+    static IList<string> GetPuzzleInput()
+    {
+        string file = Path.Combine(Environment.CurrentDirectory, "puzzle-input.txt");
+        using StreamReader sr = new StreamReader(file);
+        List<string> input = [];
+
+        while (!sr.EndOfStream)
+        {
+            input.Add(sr.ReadLine()!);
+        }
+
+        return input;
+    }
+
+    static long GetPuzzleAnswer(string input)
+    {
+        CustomList nums = new(input.Split(' ').Select(long.Parse));
+        Stack<CustomList> layers = PopulateStack(nums);
+
+        while (layers.Count > 0)
+        {
+            CustomList layer = layers.Pop();
+            long increment = layer.Last();
+            if (layers.Count == 0)
+                return increment;
+
+            CustomList nextList = layers.Peek();
+            nextList.Add(nextList.Last() + increment);
+        }
+
+        throw new Exception("This shouldn't happen");
+    }
+
+    public static Stack<CustomList> PopulateStack(CustomList firstList)
+    {
+        Stack<CustomList> layers = new();
+        layers.Push(firstList);
+
+        // populate stack
+        while (!layers.Peek().IsAllZeroes())
+        {
+            CustomList prevList = layers.Peek();
+            CustomList nextList = [];
+
+            for (int i = 0; i < prevList.Count - 1; i++)
+            {
+                nextList.Add(prevList[i + 1] - prevList[i]);
+            }
+            layers.Push(nextList);
+        }
+
+        return layers;
     }
 }
